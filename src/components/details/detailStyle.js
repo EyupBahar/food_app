34c2feeca2c47f51b7141e50f91be0be1e93ef82
7204-OMER <Eyupbhr24@gmail.com>
2021-06-23 00:13:11 +0300

    add styles

--- src/components/details/detailStyle.js
+++ src/components/details/detailStyle.js
@@ -29,3 +29,46 @@ export const DetailPart = styled.div`
     border: 1px solid red; */
   }
 `;
+
+export const ImgContainer = styled.div`
+  border: 1px solid #00adb5;
+  padding: 10px;
+  border-radius: 3px;
+  margin: 2rem;
+
+  img {
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin: 25px;
+  font-size: 3rem;
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  img {
+    width: 200px;
+  }
+`;
+
+export const IngContainer = styled.div`
+  width: 400px;
+  font-size: 1.4rem;
+  margin: 2rem;
+`;
+
+export const OtherPart = styled.div`
+  width: 400px;
+  text-align: right;
+  font-size: 1.8rem;
+  margin: 2rem;
+`;
+
