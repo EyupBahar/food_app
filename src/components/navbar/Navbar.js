abbdf4c9b859d1f7cf1445fd4624c8805c92dcf1
7204-OMER <Eyupbhr24@gmail.com>
2021-06-23 00:16:01 +0300

    add Navbar.js

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Hamburger, Logo, MenuLink, Nav, Menu } from "./navbarStyles";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Nav>
+      <Logo to="/">
+        <i>{"<Joker />"}</i>
+        <span>recipe</span>
+      </Logo>
+      <Hamburger onClick={() => setIsOpen(!isOpen)}>
+        <span />
+        <span />
+        <span />
+      </Hamburger>
+      <Menu isOpen={isOpen}>
+        <MenuLink to="/about">About</MenuLink>
+        <MenuLink
+          to={{ pathname: "https://github.com/edwardBenedict" }}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </MenuLink>
+        <MenuLink to="/login">Logout</MenuLink>
+      </Menu>
+    </Nav>
+  );
+};
+
+export default Navbar;
