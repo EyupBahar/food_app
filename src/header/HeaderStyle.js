38dcc6838719509a81cce07a7ae430ad203b3a72
7204-OMER <Eyupbhr24@gmail.com>
2021-06-22 23:34:15 +0300

    add styled.js

---
+++ src/header/HeaderStyle.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: #00adb5;
+`;
+
+export const MainHeader = styled.h2`
+  margin-top: 1rem;
+  font-size: 3rem;
+`;
+
+export const FormContainer = styled.form`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  border: 2px solid white;
+  border-radius: 3px;
+  margin: 20px;
+`;
